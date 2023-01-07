eba1788ca2ffd8a9358c31bfa44b8dc23270aaf8
Vladislav Hudasov <vladislavhudasov@yandex.ru>
2023-01-07 21:57:58 +0300

    add date Generator, compared text content, deleted //

--- src/test/java/ru/netology/cards/CardDelivery.java
+++ src/test/java/ru/netology/cards/CardDelivery.java
@@ -1,32 +1,36 @@
 package ru.netology.cards;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import ru.netology.Generator;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static ru.netology.Generator.generateDate;
 
 public class CardDelivery {
 
     @Test
     void shouldOrderCardDelivery() {
-        //Configuration.holdBrowserOpen = true;
-        //Configuration.browserSize = "900x600";
         Configuration.headless = true;
+        String planningDate = generateDate(4);
         open("http://localhost:9999/");
         $("input[placeholder='Город']").setValue("Кострома");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A" + Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue("07.02.2023");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Маша Рони");
-        //$x("//span[contains(text(), 'Мобильный')]").setValue("+79100101010"); НЕ РАБОТАЕТ НИ ОДИН ИНПУТ ЧЕРЕЗ ХPATH - выдаёт ошибку invalid element state
         $("span[data-test-id=phone] input").setValue("+79100101010");
         $("[data-test-id=agreement").click();
         $(byText("Забронировать")).click();
         $x("//div[contains(text(), 'Встреча успеш')]").should(appear, Duration.ofSeconds(11));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
